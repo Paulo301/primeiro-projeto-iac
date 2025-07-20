81dbc4bfa9029c88d6f6dcb92cec5b81d12e90cd
Paulo Victor <paulovictor1918@gmail.com>
2025-07-19 22:19:23 -0300

    feat: create datasource and outputs

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac-paulo-lemos"
+  bucket = "rocketseat-bucket-iac-paulo-lemos-${terraform.workspace}"
 
   tags = {
     Name = "Primeiro bucket"
     Iac  = true
+    Context = "${terraform.workspace}"
   }
 }
