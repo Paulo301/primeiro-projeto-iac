0df86a3fc957f09c13afb6cf5654b2c583c56098
Paulo Victor Lemos de Almeida <paulovictor1918@gmail.com>
2025-07-25 15:02:10 -0300

    feat: modularizing the s3 resource

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
   profile = "paulo-lemos-defensoria"
 }
