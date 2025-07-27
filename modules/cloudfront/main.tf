aac2445dbcd6fb6ec26ce04d755c801ae7bd3a47
Paulo Victor <paulovictor1918@gmail.com>
2025-07-27 09:58:58 -0300

    feat: more configurations for settings

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -2,20 +2,20 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   enabled = true
 
   origin {
-    origin_id = "${var.origin_id}"
-    domain_name = "${var.bucket_domain_name}"
+    origin_id   = var.origin_id
+    domain_name = var.bucket_domain_name
     custom_origin_config {
-      http_port = 80
-      https_port = 443
+      http_port              = 80
+      https_port             = 443
       origin_protocol_policy = "http-only"
-      origin_ssl_protocols = [ "TLSv1" ]
+      origin_ssl_protocols   = ["TLSv1"]
     }
   }
 
   default_cache_behavior {
     target_origin_id = var.origin_id
-    allowed_methods = [ "GET", "HEAD" ]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
     forwarded_values {
       query_string = true
       cookies {
@@ -23,9 +23,9 @@ resource "aws_cloudfront_distribution" "cloudfront" {
       }
     }
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 0
-    max_ttl = 0
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   restrictions {
@@ -37,5 +37,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
-  price_class = "PriceClass_200"
+  price_class = var.cdn_price_class
+
+  tags = var.cdn_tags
 }
--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -1,9 +1,21 @@
 variable "origin_id" {
-  type = string
+  type        = string
   description = "Id do bucket s3"
 }
 
 variable "bucket_domain_name" {
-  type = string
+  type        = string
   description = "Domínio do bucket S3"
+}
+
+variable "cdn_price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "Classe de preço do CDN"
+}
+
+variable "cdn_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags de criação"
 }
