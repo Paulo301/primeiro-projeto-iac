a530bc908d6f744a9e8d278e91928807cead7e7c
Paulo Victor Lemos de Almeida <pauloalmeida@ma.def.br>
2026-03-13 10:55:38 -0300

    feat: migrate state management to s3

--- providers.tf
+++ providers.tf
@@ -5,9 +5,24 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket  = "rocketseat-pvl-state-bucket-tf"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = "paulo-lemos-defensoria"
+  }
 }
 
 provider "aws" {
   region  = "us-east-2"
   profile = "paulo-lemos-defensoria"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
